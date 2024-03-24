e96223bfd1a3a223ca87c7ed0f0f8c54873117bd
Carlo Pignedoli <c.pignedoli@gmail.com>
2024-03-24 20:02:49 +0100

    added chmod +a for .py of spmtools and fixed a potential bug in dir c… (#23)
    * added chmod +a for .py of spmtools and fixed a potential bug in dir creation
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- before-notebook.d/72_setup_stm_tools.sh
+++ before-notebook.d/72_setup_stm_tools.sh
@@ -10,12 +10,13 @@ if [ -d "/home/jovyan/soft/" ]; then
 else
     echo "Directory does not exist. Creating"
     mkdir /home/jovyan/soft
-    mkdir /home/jovyan/soft/cp2k-spm-tools
 fi
 
 if [ -f "/home/jovyan/soft/cp2k-spm-tools/cube_from_wfn.py" ]; then
     echo "cp2k-spm-tools found"
 else
     echo "cp2k-spm-tools not found, installing"
-    git clone https://github.com/nanotech-empa/cp2k-spm-tools.git /home/jovyan/soft/cp2k-spm-tools
+    git clone https://github.com/nanotech-empa/cp2k-spm-tools.git
+    mv cp2k-spm-tools /home/jovyan/soft/
+    chmod a+x /home/jovyan/soft/cp2k-spm-tools/*py
 fi
