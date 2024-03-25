184b18ff223af9cada120688b39717f0d03c9891
dpasserone <45152275+dpasserone@users.noreply.github.com>
2024-03-25 16:37:01 +0100

    stm directories (#24)
    Co-authored-by: psd <daniele.passerone@empa.ch>

--- before-notebook.d/72_setup_stm_tools.sh
+++ before-notebook.d/72_setup_stm_tools.sh
@@ -15,7 +15,8 @@ fi
 if [ -f "/home/jovyan/soft/cp2k-spm-tools/cube_from_wfn.py" ]; then
     echo "cp2k-spm-tools found"
 else
-    echo "cp2k-spm-tools not found, installing"
+    echo "cp2k-spm-tools not found, installing after removing dir"
+    rm -Rf /home/jovyan/soft/cp2k-spm-tools /home/jovyan/cp2k-spm-tools
     git clone https://github.com/nanotech-empa/cp2k-spm-tools.git
     mv cp2k-spm-tools /home/jovyan/soft/
     chmod a+x /home/jovyan/soft/cp2k-spm-tools/*py
