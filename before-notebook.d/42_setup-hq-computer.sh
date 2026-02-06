18492973677aaf3cab1b73c5a4c003a16fe623b1
Carlo Antonio Pignedoli <c.pignedoli@gmail.com>
2026-02-06 09:07:15 +0100

    bug fix, when localhost-legacy already present

--- before-notebook.d/42_setup-hq-computer.sh
+++ before-notebook.d/42_setup-hq-computer.sh
@@ -2,24 +2,29 @@
 
 set -x
 
-# Disable and rename the default 'localhost' computer to 'localhost-legacy'.
-# This is necessary because we will create a new 'localhost' computer
-# configured with HyperQueue as the job management system.
-verdi computer disable localhost aiida@localhost
-verdi computer relabel localhost localhost-legacy
+if ! verdi computer list -a | awk '{print $1}' | grep -qx "localhost-legacy"; then
 
+    # Disable and rename the default localhost
+    verdi computer disable localhost aiida@localhost
+    verdi computer relabel localhost localhost-legacy
 
-# computer
-verdi computer show ${COMPUTER_LABEL} || verdi computer setup        \
-  --non-interactive                                               \
-  --label "${COMPUTER_LABEL}"                                        \
-  --description "local computer with hyperqueue scheduler"        \
-  --hostname "localhost"                                          \
-  --transport core.local                                          \
-  --scheduler hyperqueue                                          \
-  --work-dir /home/${NB_USER}/aiida_run/                          \
-  --mpirun-command "mpirun -np {num_cpus}"
+    # Setup new localhost with HyperQueue
+    verdi computer setup        \
+      --non-interactive                                                   \
+      --label "${COMPUTER_LABEL}"                                         \
+      --description "local computer with hyperqueue scheduler"            \
+      --hostname "localhost"                                              \
+      --transport core.local                                              \
+      --scheduler hyperqueue                                              \
+      --work-dir /home/${NB_USER}/aiida_run/                               \
+      --mpiprocs-per-machine 2                                             \
+      --mpirun-command "mpirun -np {num_cpus}"
 
-verdi computer configure core.local "${COMPUTER_LABEL}"              \
-  --non-interactive                                               \
-  --safe-interval 5.0
+    # Configure transport (ONLY when setup happened)
+    verdi computer configure core.local "${COMPUTER_LABEL}"               \
+      --non-interactive                                                   \
+      --safe-interval 5.0
+
+else
+    echo "localhost-legacy already exists → skipping computer setup & configure"
+fi
